9684d5c924bf33fabd33f0d3c579375e8160eac8
seung <seung@gmail.com>
2021-05-31 20:16:52 -0400

    Apollo client setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-
+import React from "react";
+import {HashRouter as Router, Route} from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail"
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  return <Router>
+    <Route exact path="/" component={Home}></Route>
+    <Route path="/:id" component={Detail}></Route>
+  </Router>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import { render }  from 'react-dom';
+import { ApolloProvider } from "@apollo/client/react";
 import App from './components/App';
+import client from './Apollo';
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <ApolloProvider client={client}>
+    <App></App>
+    </ApolloProvider>,
   document.getElementById('root')
 );
